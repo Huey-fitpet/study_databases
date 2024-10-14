2e4a39ff7c9cecd2901789b942548058c6e0feea
Huey <demonic0319@gmail.com>
2024-10-14 02:25:30 +0000

    feat: update_recode update

--- SQLs/notice/update_recode_DML.sql
+++ SQLs/notice/update_recode_DML.sql
@@ -13,10 +13,16 @@ CREATE TABLE `NOTICE_VISITOR` (
 */
 -- syntex 참조
 -- UPDATE table_name
--- SET column1 = value1, column2 = value2, ...
--- WHERE condition;
+-- SET column1 = value1, column2 = value2, ... -- pk는 못바꾼다고 생각하자
+-- WHERE condition; -- 주로 unique 한 값
 
 -- update notice_contents
-UPDATE table_name
-SET column1 = value1, column2 = value2, ...
-WHERE condition;
+UPDATE `NOTICE_CONTENTS`
+SET `CONTENTS` = '이벤트 안내 수정', `WRITER` = '관리자'
+WHERE `PK_A` = 'A002'
+;
+
+UPDATE `NOTICE_VISITOR`
+SET `VISITOR` = '이상훈'
+WHERE `FK_A` = 'A002'
+;
