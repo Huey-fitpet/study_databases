0604a63bed280b162c5e19b472768523dce8c476
Huey <demonic0319@gmail.com>
2024-10-15 05:56:23 +0000

    feat: range and equal update

---
+++ SQLs/selectwhereRangeAndEqual.sql
@@ -0,0 +1,4 @@
+--Number of Records: 2
+SELECT * 
+FROM Customers
+WHERE (CustomerID >= 50 AND CustomerID <= 89) AND City = 'London'
--- SQLs/selectwherenotcountry.sql
+++ SQLs/selectwherenotcountry.sql
@@ -3,3 +3,10 @@ SELECT CustomerName, City, Country
 FROM Customers
 WHERE NOT (Country = 'Germany') AND NOT (Country = 'USA')
 ;
+SELECT CustomerName, City, Country 
+FROM Customers
+WHERE Country <> 'Germany' AND Country <> 'USA'
+;
+SELECT CustomerName, City, Country 
+FROM Customers
+WHERE Country NOT IN ('Germany','USA');
