43cbca1ff8c2ad8c15c4bf771ab0dc365258913c
Huey <demonic0319@gmail.com>
2024-10-16 08:59:57 +0000

    feat: total price update

---
+++ SQLs/CustomersOrderTotalPrices.sql
@@ -0,0 +1,31 @@
+/*
+
+- Table : Customers, Orders 등 
+- 조건 : 대상 고객이 지불해야 하는 총 금액 순으로 Desc - 
+- 주문 없는 고객도 이름과 나라는 출력 [대상 고객] 
+- SELECT * FROM Customers where CustomerID IN (5,10,22)
+
+
+Number of Records: 3
+CustomerID	    CustomerName	                    Country	TOTAL_PRICE
+    5	    Berglunds snabbköp	                    Sweden	28355.75
+    10	    Bottom-Dollar Marketse	                Canada	24429.15
+    22	    FISSA Fabrica Inter. Salchichas S.A.	Spain	
+
+*/
+
+
+SELECT Total.CustomerID, Total.CustomerName, Total.Country, SUM(Total.total) AS TOTAL_PRICE
+FROM (
+SELECT C.CustomerID, C.CustomerName, C.Country, P.Price, OD.Quantity, P.Price * OD.Quantity AS total
+FROM   (SELECT CustomerID, CustomerName, Country 
+		FROM Customers
+		WHERE CustomerID IN (5,10,22)) AS C 
+        LEFT JOIN Orders AS O
+        ON C.CustomerID = O.CustomerID 
+        LEFT JOIN OrderDetails AS OD 
+        ON OD.OrderID = O.OrderID
+        LEFT JOIN Products AS P
+        ON OD.ProductID = P.ProductID
+        ) AS Total
+ GROUP BY Total.CustomerName
